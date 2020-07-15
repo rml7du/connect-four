7b589b67ba753cb7612df5f3bfc81d85010e73e8
Robert Lee <rml7du@gmail.com>
2020-07-15 14:43:33 -0600

    minor edits to player test and got the board tests working

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -1,15 +1,20 @@
 class Player
-    attr_accessor :name, :piece
+    attr_accessor :name, :piece, :player_count
     @@player_count = 0
 
     def initialize(name)
-        @@player_count =+ 1
+        @@player_count += 1
         @name = name
         @piece = pick_piece()
     end
 
+    def self.player_count
+        # Return the value of this variable
+        @@player_count
+    end
+
     def pick_piece()
-        @player_count == 1 ? "O" : "X" #can change this to make the pieces more interesting
+        @@player_count == 1 ? "O" : "X" #can change this to make the pieces more interesting
     end
 end
 
@@ -17,5 +22,83 @@ class Game
 end
 
 class Board
-end
+    attr_accessor :array
+    def initialize()
+        @array = create_board()
+    end
 
+    def create_board()
+        array = Array.new(6) { Array.new(7) { ' '}}
+    end
+
+    def print_board()
+        board.array.each {|x| print "#{x}\n"}
+        puts ""
+    end
+
+    def check_winner(player)
+        # horizontalCheck 
+        j = 0
+        i = 0
+        while j < 7-3 #width 
+            while i < 6
+                return true if @array[i][j] == player.piece && @array[i][j+1] == player.piece && @array[i][j+2] == player.piece && @array[i][j+3] == player.piece
+                i +=1
+            end
+            j += 1    
+        end
+
+        # verticalCheck
+        j = 0
+        i = 0
+        while i< 7-3 
+            while j < 6
+                return true if @array[i][j] == player.piece && @array[i+1][j] == player.piece && @array[i+2][j] == player.piece && @array[i+3][j] == player.piece
+                j+=1
+            end
+            i+=1
+        end
+        # ascendingDiagonalCheck 
+        j = 3
+        i = 3
+        while i < 7
+            while j < 6
+                return true if @array[i][j] == player.piece && @array[i-1][j+1] == player.piece && @array[i-2][j+2] == player.piece && @array[i-3][j+3] == player.piece
+                j+=1
+            end
+            i+=1
+        end
+        # descendingDiagonalCheck
+        j = 3
+        i = 3
+        while i < 7
+            while j < 6
+                return true if @array[i][j] == player.piece && @array[i-1][j-1] == player.piece && @array[i-2][j-2] == player.piece && @array[i-3][j-3] == player.piece                
+                j+=1
+            end
+            i+=1
+        end
+        return false;
+    end
+end
+=begin
+board = Board.new
+player = Player.new("player")
+player2 = Player.new("player2")
+puts player.piece
+puts player2.piece
+puts ""
+#puts player_count
+puts Player.player_count
+
+board.array[0][0] = "O"
+board.array[0][1] = "O"
+board.array[0][2] = "O"
+board.array[0][3] = "O"
+
+
+puts""
+
+puts board.array[0][0] == player.piece
+puts board.check_winner(player)
+=end
--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -1,15 +1,21 @@
 require "./lib/connect_four"
 describe Player do
 
+    it "store player 1 piece" do
+        player = Player.new("player1")
+        expect(player.piece).to eql("O")
+    end
+    
     it "create a player" do
         player = Player.new("player1")
         expect(player.name).to eql("player1")
     end
 
-    it "store their piece" do
-        player = Player.new("player1")
-        expect(player.piece).to eql("X")
+    it "store player 2 piece" do
+        player2 = Player.new("player2")
+        expect(player2.piece).to eql("X")
     end
+
 end
 
 describe Game do
@@ -21,9 +27,27 @@ describe Game do
 end
 
 describe Board do
-    describe "#somefunction" do
-        it "shoudl do something" do
-            expect().to eql()
+    describe "#buildboard" do
+        it "should create a 7x6(high) grid" do
+            test = Board.new
+            expect(test.array.flatten.length).to eql(42)
+        end
+
+        it "the grid should be empty" do
+            test = Board.new
+            expect(test.array.flatten.all? {|value| value == ' ' }).to equal(true)
+        end
+    end
+
+    describe "#check_winner" do
+        it "should return true if there are 4 pieces of the same 'color' in a row" do
+            test = Board.new
+            player = Player.new("player1")
+            test.array[0][0] = "X"
+            test.array[0][1] = "X"
+            test.array[0][2] = "X"
+            test.array[0][3] = "X"
+            expect(test.check_winner(player)).to eql(true)
         end
     end
 end
