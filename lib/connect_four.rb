dcf27983958feca018319af3d398c617f0a5296f
Robert Lee <rml7du@gmail.com>
2020-07-16 13:05:06 -0600

    had to revamp the check winner test as it was not working on edge cases. fully functional connect 4 but some of the tests are not complete :(

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -25,87 +25,83 @@ class Board
     end
 
     def create_board()
-        array = Array.new(6) { Array.new(7) { '| . |'}}
+        array = Array.new(6) { Array.new(7) { '.'}} 
     end
 
     def print_board()
-        puts "----------select a column---------"
-        print "  1    2    3    4    5    6    7\n"
-        @array.each {|x| print "#{x.join('')}\n"}
+        puts "----select a column-----"
+        print " 1 2 3 4 5 6 7\n"
+        @array.each {|x| print "|#{x.join('|')}|\n"}
         puts ""
     end
 
     def place_piece(player, x) #drop piece in correct column
-        return false if @array[0][x] != "| . |"
-        if @array[5][x] == "| . |"
-            @array[5][x] = "| #{player.piece} |" 
-            return true
-        elsif @array[4][x] == "| . |"
-            @array[4][x] = "| #{player.piece} |" 
-            return true
-        elsif @array[3][x] == "| . |"
-            @array[3][x] = "| #{player.piece} |"
-            return true
-        elsif @array[2][x] == "| . |"
-            @array[2][x] = "| #{player.piece} |" 
-            return true
-        elsif @array[1][x] == "| . |"
-            @array[1][x] = "| #{player.piece} |" 
-            return true
-        elsif @array[0][x] == "| . |"
-            @array[0][x] = "| #{player.piece} |" 
-            return true
+        if @array[5][x] == "."
+            @array[5][x] = "#{player.piece}" 
+        elsif @array[4][x] == "."
+            @array[4][x] = "#{player.piece}" 
+        elsif @array[3][x] == "."
+            @array[3][x] = "#{player.piece}"
+        elsif @array[2][x] == "."
+            @array[2][x] = "#{player.piece}" 
+        elsif @array[1][x] == "."
+            @array[1][x] = "#{player.piece}" 
+        elsif @array[0][x] == "."
+            @array[0][x] = "#{player.piece}" 
         end
     end
 
     def check_winner(player)
-        # horizontalCheck 
-        j = 0
-        i = 0
-        while j < 7-3 #width 
-            while i < 6
-                return true if @array[i][j] == "| #{player.piece} |" && @array[i][j+1] == "| #{player.piece} |" && @array[i][j+2] == "| #{player.piece} |" && @array[i][j+3] == "| #{player.piece} |"
-                i +=1
+        #horizontal check
+        h = 0
+      
+        while h < 6
+            w = 0
+            while w < 7 - 3
+                return true if @array[h][w] == "#{player.piece}" && @array[h][w+1] == "#{player.piece}" && @array[h][w+2] == "#{player.piece}" && @array[h][w+3] == "#{player.piece}"
+                w += 1
             end
-            j += 1    
+            h+=1
         end
 
-        # verticalCheck
-        j = 0
-        i = 0
-        while i< 7-3 
-            while j < 6
-                return true if @array[i][j] == "| #{player.piece} |" && @array[i+1][j] == "| #{player.piece} |" && @array[i+2][j] == "| #{player.piece} |" && @array[i+3][j] == "| #{player.piece} |"
-                j+=1
+        #vertical check
+        h = 0
+        
+        while h < 6 - 3
+            w = 0
+            while w < 7
+                return true if @array[h][w] == "#{player.piece}" && @array[h+1][w] == "#{player.piece}" && @array[h+2][w] == "#{player.piece}" && @array[h+3][w] == "#{player.piece}" 
+                w+=1
             end
-            i+=1
+            h+=1
         end
-        # ascendingDiagonalCheck 
-        j = 3
-        i = 3
-        while i < 7
-            while j < 6
-                return true if @array[i][j] == "| #{player.piece} |" && @array[i-1][j+1] == "| #{player.piece} |" && @array[i-2][j+2] == "| #{player.piece} |" && @array[i-3][j+3] == "| #{player.piece} |"
-                j+=1
+
+        #ascending diagnol check
+        h = 3 #because limitations on ascending diagnol
+        while h < 6
+            w = 0 
+            while w < 7-3
+                return true if @array[h][w] == "#{player.piece}" && @array[h-1][w+1] == "#{player.piece}" && @array[h-2][w+2] == "#{player.piece}" && @array[h-3][w+3] == "#{player.piece}"
+                w+=1
             end
-            i+=1
+            h+=1
         end
-        # descendingDiagonalCheck
-        j = 3
-        i = 3
-        while i < 7
-            while j < 6
-                return true if @array[i][j] == "| #{player.piece} |" && @array[i-1][j-1] == "| #{player.piece} |" && @array[i-2][j-2] == "| #{player.piece} |" && @array[i-3][j-3] == "| #{player.piece} |"                
-                j+=1
+
+        #descending diagnol check
+        h = 0
+        while h < 3
+            w = 0 
+            while w < 7 - 3
+                return true if @array[h][w] == "#{player.piece}" && @array[h+1][w+1] == "#{player.piece}" && @array[h+2][w+2] == "#{player.piece}" && @array[h+3][w+3] == "#{player.piece}"  
+                w+=1
             end
-            i+=1
-        end
-        return false;
+            h+=1
+        end   
     end
 end
 
 class Game
-    attr_accessor :turn
+    attr_accessor :turn, :board
     def initialize()
         @turn = 1
         @player1 = Player.new("player1")
@@ -115,7 +111,7 @@ class Game
     end
     
     def gameplay()
-        while @turn < 1 #total possible turns
+        while @board.array.flatten.any? { |x| x == '.' } #total possible turns
             if @turn % 2 == 1
                 current_player = @player1
             else
@@ -123,18 +119,23 @@ class Game
             end
             player_turn(current_player)
             if @board.check_winner(current_player)
-                puts "WINNER! #{current_player} wins the game"
                 @board.print_board
+                puts "WINNER! #{current_player.name} wins the game"
                 break
             end
             @turn +=1
         end
+        if @board.array.flatten.none? {|x| x == '.'}
+            @board.print_board
+            puts "ITS A TIE!"
+        end    
     end
 
     def player_turn(player)
         @board.print_board
         puts "select a column 1-7"
-        selection(player) 
+        while !selection(player)
+        end
     end
 
     def selection(player)
@@ -142,18 +143,17 @@ class Game
         if !('0'..'6').include?("#{selection}")
             puts "invalid selection, please try again"
             return false
-        elsif @board.place_piece(player,selection) == false
+        elsif @board.array[0][selection] != "."
             puts "that space is taken, try again"
             return false
-        else 
-            @board.place_piece(player,selection)
+        else @board.place_piece(player,selection)
             return true
         end
     end
 end
 
 
-#game = Game.new
+game = Game.new
 
 =begin
 aries = "\u2648".encode('utf-8')
--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -16,11 +16,107 @@ describe Player do
     end
 end
 
+
+
+describe Board do
+    describe "#buildboard" do
+        it "should create a 7x6(high) grid" do
+            test = Board.new
+            expect(test.array.flatten.length).to eql(42)
+        end
+
+        it "the grid should be empty" do
+            test = Board.new
+            expect(test.array.flatten.all? {|value| value == '.' }).to equal(true)
+        end
+    end
+
+    describe "#check_winner" do
+        it "should return true if there are 4 pieces of the same 'color' in a horizontal order" do
+            test = Board.new
+            player = Player.new("player1")
+            test.array[0][3] = "#{player.piece}" 
+            test.array[0][4] = "#{player.piece}" 
+            test.array[0][5] = "#{player.piece}" 
+            test.array[0][6] = "#{player.piece}" 
+            test.print_board()
+            expect(test.check_winner(player)).to eql(true)
+        end
+
+        it "should return true if there are 4 pieces of the same 'color' in a horizontal order" do
+            test = Board.new
+            player = Player.new("player1")
+            test.array[5][1] = "#{player.piece}" 
+            test.array[5][2] = "#{player.piece}" 
+            test.array[5][3] = "#{player.piece}" 
+            test.array[5][4] = "#{player.piece}" 
+            test.print_board()
+            expect(test.check_winner(player)).to eql(true)
+        end
+
+        it "should return true if there are 4 pieces of the same 'color' in vertical order" do
+            test = Board.new
+            player = Player.new("player1")
+            test.array[0][5] = "#{player.piece}" 
+            test.array[1][5] = "#{player.piece}" 
+            test.array[2][5] = "#{player.piece}" 
+            test.array[3][5] = "#{player.piece}" 
+            test.print_board()
+            expect(test.check_winner(player)).to eql(true)
+        end
+
+        it "should return true if there are 4 pieces of the same 'color' in descending diagonol order" do
+            test = Board.new
+            player = Player.new("player1")
+            test.array[0][0] = "#{player.piece}" 
+            test.array[1][1] = "#{player.piece}" 
+            test.array[2][2] = "#{player.piece}" 
+            test.array[3][3] = "#{player.piece}" 
+            test.print_board()
+            expect(test.check_winner(player)).to eql(true)
+        end
+
+        it "should return true if there are 4 pieces of the same 'color' in ascending diagonol order" do
+            test = Board.new
+            player = Player.new("player1")
+            test.array[5][0] = "#{player.piece}" 
+            test.array[4][1] = "#{player.piece}" 
+            test.array[3][2] = "#{player.piece}" 
+            test.array[2][3] = "#{player.piece}" 
+            test.print_board()
+            expect(test.check_winner(player)).to eql(true)
+        end
+
+        it "should return true if there are 4 pieces of the same 'color' in ascending diagonol order" do
+            test = Board.new
+            player = Player.new("player1")
+            test.array[5][3] = "#{player.piece}" 
+            test.array[4][4] = "#{player.piece}" 
+            test.array[3][5] = "#{player.piece}" 
+            test.array[2][6] = "#{player.piece}" 
+            test.print_board()
+            expect(test.check_winner(player)).to eql(true)
+        end
+    end
+end
+
+=begin
+
 describe Game do
     describe "#player_selection" do
         it "have the player make a selection" do
-            test_game = Game.new
-            expect().to eql()
+            test = Game.new
+            allow_any_instance_of(Object).to receive(:gets).and_return "1\n"
+            expect(test.board.array[0][0]).to eql("| X |")
+        end
+
+        it "should return false when an invalid selection is made" do
+            test = GamePlay.new()
+            allow_any_instance_of(Object).to receive(:gets).and_return "10\n"
+            
+
+            expect(board.selection(player1)).to eql(false)
+
         end
 
         it "should place the players piece on the board correctly" do
@@ -34,29 +130,4 @@ describe Game do
         end
     end
 end
-
-describe Board do
-    describe "#buildboard" do
-        it "should create a 7x6(high) grid" do
-            test = Board.new
-            expect(test.array.flatten.length).to eql(42)
-        end
-
-        it "the grid should be empty" do
-            test = Board.new
-            expect(test.array.flatten.all? {|value| value == '| . |' }).to equal(true)
-        end
-    end
-
-    describe "#check_winner" do
-        it "should return true if there are 4 pieces of the same 'color' in a row" do
-            test = Board.new
-            player = Player.new("player1")
-            test.array[0][0] = "| #{player.piece} |" 
-            test.array[0][1] = "| #{player.piece} |" 
-            test.array[0][2] = "| #{player.piece} |" 
-            test.array[0][3] = "| #{player.piece} |" 
-            expect(test.check_winner(player)).to eql(true)
-        end
-    end
-end
+=end
